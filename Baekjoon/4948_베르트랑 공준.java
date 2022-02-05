1ccd478a1c8db71ffb3f41c79b18b21dca20e0ef
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-02-05 12:00:00 +0900

    수학, 소수판별

---
+++ Baekjoon/4948_베르트랑 공준.java
@@ -0,0 +1,34 @@
+package Silver2;
+
+import java.io.*;
+
+public class s4948 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			if(n==0) break;
+			int count=0;
+			
+			for(int j=n+1;j<=n*2;j++) { //n<소수<=2n
+				if(isPrime(j)==true) 
+					count++;
+			}
+			sb.append(count+"\n");
+		}
+		
+		bw.write(sb+""); bw.flush();
+	}
+	static boolean isPrime(int num) {
+		if(num==1) return false;
+		for(int i=2;i*i<=num;i++) {
+			if(num%i==0) return false;
+		}
+		return true;
+	}
+
+}
