4a5985d7399c5e3ca86e8b22b0c8880dc31c50f3
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-02-06 15:04:24 +0900

    수학

---
+++ Baekjoon/2163_초콜릿 자르기.java
@@ -0,0 +1,14 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class b2163 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); //1<=n<=300
+		int m = sc.nextInt(); //1<=m<=300
+		
+		System.out.print(n*m-1);
+	}
+}
