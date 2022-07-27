0fe7d20880dd23ee133ed423f29bacc06a1e1951
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-07-27 22:41:59 +0900

    그리디

---
+++ Baekjoon/18870_좌표 압축.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        //O(n)
+        ArrayList<Integer> input = new ArrayList(n);
+        for(int i=0;i<n;i++) {
+            input.add(Integer.parseInt(st.nextToken()));
+        }
+
+        HashSet<Integer> inputSet = new HashSet<>(input); //중복값 제거
+        Integer[] newArr = inputSet.toArray(new Integer[0]);
+        Arrays.sort(newArr);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<inputSet.size();i++) {
+            map.put(newArr[i],i);
+        }
+
+        //O(n)
+        for(int i=0;i<n;i++) {
+            sb.append(map.get(input.get(i)) + " ");
+        }
+        bw.write(sb+""); bw.flush();
+
+    }
+}
